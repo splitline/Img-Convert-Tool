b2bf43b6839f7aad5841e3ae35b8fb794c06f63b
黃志仁 <tbsthitw@gmail.com>
2017-08-30 08:15:44 +0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -7,14 +7,16 @@ def listDir(rootDir):
     list_dirs = os.walk(rootDir)
     for root, dirs, files in list_dirs:
         print("[dir] " + root)
-        for f in files:
-            if f == "texture.png":
-                print ("[file] " + os.path.join(root, f))
-                img = Image.open(root + "/texture.png")
-                with codecs.open(root + "/texture.json", 'r', 'utf-8-sig') as data_file:
-                    data = json.load(data_file)
-                    for i in range(len(data["SubTexture"])):
-                        crop_img = img.crop((data["SubTexture"][i]["x"], data["SubTexture"][i]["y"], data["SubTexture"][i]["x"]+data["SubTexture"][i]["width"], data["SubTexture"][i]["y"]+data["SubTexture"][i]["height"]))
-                        file_name = data["SubTexture"][i]["name"]
-                        crop_img.save(root + "/" + file_name[file_name.rfind('/'):], "PNG")
-listDir("./")
+        if "texture.png" in files:
+            print ("[file] " + os.path.join(root, "texture.png"))
+            img = Image.open(root + "/texture.png")
+            with codecs.open(root + "/texture.json", 'r', 'utf-8-sig') as data_file:
+                data = json.load(data_file)
+                for i in range(len(data["SubTexture"])):
+                    crop_img = img.crop((data["SubTexture"][i]["x"], data["SubTexture"][i]["y"], data["SubTexture"][i]["x"]+data["SubTexture"][i]["width"], data["SubTexture"][i]["y"]+data["SubTexture"][i]["height"]))
+                    file_name = data["SubTexture"][i]["name"]
+                    crop_img.save(root + "/" + file_name[file_name.rfind('/'):], "PNG")
+
+
+rootDir = input("Input Root Dir > ")
+listDir(rootDir.strip())
